7ca27fe042b3e98d2b5b6d2d7dde128f938901fa
胡仕杰 <63051320+MladenSrB@users.noreply.github.com>
2022-10-05 15:06:43 +0800

    Update 1005.cpp

--- 1005.cpp
+++ 1005.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 
 int main(){
-    int sc1, sc2, sc3, sum;
+    int sc1, sc2, sc3, sc4, sc5, sum;
     float ave;
-    cin >> sc1 >> sc2 >> sc3;
+    cin >> sc1 >> sc2 >> sc3 >> sc4 >> sc5;
 
-    sum = sc1 + sc2 + sc3;
-    ave= sum / 3.0;
+    sum = sc1 + sc2 + sc3 + sc4 + sc5;
+    ave= sum / 5.0;
 
     cout << "總分 = " <<  sum << endl;
     cout << "平均 = " <<  ave;
